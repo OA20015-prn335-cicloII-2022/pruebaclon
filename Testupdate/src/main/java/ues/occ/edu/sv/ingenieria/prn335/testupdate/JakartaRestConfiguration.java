2b6715c844a8ac9f1eaa3ea3b989ff65d9b66e4a
OA20015-prn335-cicloII-2022 <112209092+OA20015-prn335-cicloII-2022@users.noreply.github.com>
2022-10-14 23:28:59 -0600

    Add files via upload

---
+++ Testupdate/src/main/java/ues/occ/edu/sv/ingenieria/prn335/testupdate/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package ues.occ.edu.sv.ingenieria.prn335.testupdate;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ Testupdate/src/main/java/ues/occ/edu/sv/ingenieria/prn335/testupdate/resources/JakartaEE8Resource.java
@@ -0,0 +1,20 @@
+package ues.occ.edu.sv.ingenieria.prn335.testupdate.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("rest")
+public class JakartaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
